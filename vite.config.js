370a8b9a2f828aaf7cd8ba26095a0dd63cd7944c
Abdul Kareem <41697424+Salmanabdul786@users.noreply.github.com>
2024-09-23 15:03:41 +0530

    Build1

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import filterReplace from 'vite-plugin-filter-replace';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
         chunkSizeWarningLimit: 1600
-    }
+    },
+  plugins: [
+      // Workaround warning with lottie - https://github.com/airbnb/lottie-web/issues/2927
+      filterReplace([
+        {
+          filter: ['node_modules/lottie-web/build/player/lottie.js'],
+          replace: {
+            from: 'eval(\'[function _expression_function(){\' + val + \';scoped_bm_rt=$bm_rt}]\')[0]',
+            to: '(new Function(\'scoped_bm_rt\', val + \';return $bm_rt;\'))()'
+          }
+        }
+      ]),
+    ],
 })
